51ee4f5c61c9411ae9c539de7de1dde32917cbcb
arnaud <arnaud33200@gmail.com>
2018-10-20 16:49:02 -0400

    update contact page; udpate footer content

--- contact.php
+++ contact.php
@@ -4,13 +4,13 @@ echo file_get_contents( "mainPhp/main_layout_top.php" );
 
 	<h1 class="uk-heading-primary">Contact</h1>
 
-<ul class="uk-list uk-list-bullet">
-    <li>Facebook: facebook.com/tristanzababand</li>
-	<li>Twitter: twitter.com/tristan_zaba</li>
-	<li>YouTube: youtube.com/user/zabaofficial</li>
-	<li>Soundcloud: soundcloud.com/tristan-zaba</li>
-	<li>Bandcamp: tristanzaba.bandcamp.com</li>
-	<li>Manitoba Music: manitobamusic.com/zaba</li>
+<ul class="uk-list">
+    <li class="uk-text-lead">Mail - <a class="uk-link-muted" href="mailto:zabaofficial@gmail.com">zabaofficial@gmail.com</a></li>
+    <li class="uk-text-lead">Facebook - <a class="uk-link-muted" target="_blank" href="https://facebook.com/tristanzababand">tristanzababand</a></li>
+	<li class="uk-text-lead">Twitter - <a class="uk-link-muted" target="_blank" href="https://twitter.com/tristan_zaba">tristan_zaba</a></li>
+	<li class="uk-text-lead">YouTube - <a class="uk-link-muted" target="_blank" href="https://youtube.com/user/zabaofficial">zabaofficial</a></li>
+	<li class="uk-text-lead">Soundcloud - <a class="uk-link-muted" target="_blank" href="https://soundcloud.com/tristan-zaba">tristan-zaba</a></li>
+	<li class="uk-text-lead">Bandcamp - <a class="uk-link-muted" target="_blank" href="https://tristanzaba.bandcamp.com">tristanzaba</a></li>
 </ul>
 
 <?php echo file_get_contents( "mainPhp/main_layout_bottom.php" ); ?>
--- mainPhp/main_layout_bottom.php
+++ mainPhp/main_layout_bottom.php
@@ -6,10 +6,19 @@
 
 	</div>
 
-	<footer uk-sticky="bottom: true" style="background: #d8dada; padding: 20px">
-		<p style="display: -webkit-inline-box;">Contact info: </p>
-		<span id="footer-icon" uk-icon="icon: mail; ratio: 1"></span>
-		<span id="footer-icon" uk-icon="icon: facebook; ratio: 1"></span>
+	<footer uk-sticky="bottom: true" style="background: #d8dada; padding: 10px 20px 10px 20px">
+
+		<div id="developer-info" style="float: right;">
+			<em style="display: block;" class="uk-text-meta uk-text-right">developed by</br>Arnaud.L</em>			
+		</div>
+
+		<em style="display: block;" class="uk-text-meta">Tristan Zaba 2018</em>
+		<a id="footer-icon" uk-icon="icon: mail; ratio: 1" href="mailto:zabaofficial@gmail.com"></a>
+		<a id="footer-icon" uk-icon="icon: facebook; ratio: 1" target="_blank" href="https://facebook.com/tristanzababand"></a>
+		<a id="footer-icon" uk-icon="icon: twitter; ratio: 1" target="_blank" href="https://twitter.com/tristan_zaba"></a>
+		<a id="footer-icon" uk-icon="icon: youtube; ratio: 1" target="_blank" href="https://youtube.com/user/zabaofficial"></a>
+		<a id="footer-icon" uk-icon="icon: soundcloud; ratio: 1" target="_blank" href="https://soundcloud.com/tristan-zaba"></a>
+		
 
 	</footer>
 
