143c69e2044a400c40d77711a091a14400c5c77b
arnaud <arnaud33200@gmail.com>
2018-10-21 13:48:05 -0400

    update content for news page

--- musical-works.php
+++ musical-works.php
@@ -2,73 +2,58 @@
 echo file_get_contents( "mainPhp/main_layout_top.php" ); 
 ?>
 
-	<h1 class="uk-heading-primary">Musical Works</h1>
+	<h1 class="uk-heading-primary">Works</h1>
 
-	
-	<h2 class="uk-heading-line"><span>Chamber Music</span></h2>
-
+	<h2 class="uk-heading-line"><span>Chamber</span></h2>
 	<ul class="uk-list uk-list-striped">
-		<li><b>Perdita</b> – Mezzo voice, Bass voice, Flute, Cello, Piano (2015) or Mezzo voice, Bass voice, Sop sax, Cello, Double-bass, Piano	(2016) </li>
-		<li><b>Altercations</b> – Violin, Cello, Piano (2015)</li>
-		<li><b>Channeling</b> – Clarinet, Flute, Prepared electric guitar, Live electronics (2015)</li>
-		<li><b>Blow Thou Winter Wynd</b> – Sop voice, Bass voice, Flute, Clarinet, Viola, Piano (2016) or Sop voice, Flute, Clarinet, Sop sax, Cello, Piano (2016) </li>
-		<li><b>Bow I.E.</b> – String Quartet (2016) </li>
-		<li><b>The Chromacluster Boogie</b> – Flute, Clarinet, Violin, Cello, Piano (2016) </li>
-		<li><b>Stray Transmissions</b> – Alto sax (2016) </li>
-		<li><b>Jigsaw Variations</b> - Bassoon, Cello, Piano (2017)</li>
-		<li><b>Interspatial Slipstreams</b> – Piano (2018)</li>
-		<li><b>Four Piano Études</b> - Piano (2018)</li>
-		<li><b>The Well Powered Clavichord</b> - Clavichord, Live electronics (2018)</li>
+		<li>Matryoshki and a Blue Vase - Piano (2018)</li>
+		<li>Four Piano Études - Piano (2018)</li>
+		<li>Interspatial Slipstreams – Piano (2018)</li>
+		<li>Jigsaw Variations - Bassoon, Cello, Piano (2017)</li>
+		<li>Stray Transmissions – Saxophone (2016)</li>
+		<li>The Chromacluster Boogie – Flute, Clarinet, Violin, Cello, Piano (2016)</li>
+		<li>Bow I.E. – String Quartet (2016)</li>
+		<li>Blow Thou Winter Wynd – Soprano, Bass, Flute, Clarinet, Viola, Piano (2016)</li>
+		<li>Channeling (co-composed) – Flute, Clarinet, Prepared Electric Guitar, Live</li>
 	</ul>
 
-<h2 class="uk-heading-line"><span>Electroacoustic</span></h2>
-
+	<h2 class="uk-heading-line"><span>Electronics (2015)</span></h2>
 	<ul class="uk-list uk-list-striped">
-		<li>Paraworlds I (2016) </li>
-		<li>Paraworlds III (2017)</li>
-		<li>The Void Gazes Back (2017)</li>
+		<li>Altercations – Violin, Cello, Piano (2015)</li>
+		<li>Perdita – Mezzo-Soprano, Bass, Flute, Cello, Piano (2015)</li>
+	</ul>
+
+	<h2 class="uk-heading-line"><span>Choral</span></h2>
+	<ul class="uk-list uk-list-striped">
+		<li>Like a Geometer - SATB choir, Organ (2018) or SATB choir, Live electronics</li>
+		<li>Expansion – SATB choir, Snare drum (2015)</li>
+	</ul>
+
+	<h2 class="uk-heading-line"><span>Electroacoustic</span></h2>
+	<ul class="uk-list uk-list-striped">
 		<li>Inferno/Purgatorio/Paradiso (2018)</li>
+		<li>The Void Gazes Back (2017)</li>
+		<li>Paraworlds III (2017)</li>
+		<li>Paraworlds I (2016)</li>
 	</ul>
 
+	<h2 class="uk-heading-line"><span>Large Ensemble</span></h2>
+	<ul class="uk-list uk-list-striped">
+		<li>Eurobsession - Symphony Orchestra (2017)</li>
+		<li>Paraworlds II – Symphony Orchestra (2016)</li>
+		<li>Sun Shower Concertino - Saxophone Quartet with Wind Ensemble (2016)</li>
+	</ul>
 
-<h2 class="uk-heading-line"><span>Vocal Music</span></h2>
-
-<ul class="uk-list uk-list-striped">
-	<li>A Dream, Art son – Tenor voice, Piano (2015) or Bass voice, Piano (2016) </li>
-	<li>Farewell to Nova Scotia, Chamber Opera – Tenor voice, Violin, Piano (2016) </li>
-	<li>I Hope I Shall Arrive Soon, Chamber Opera – Soprano voice, Mezzo voice, Counter-Tenor voice, Tenor voice, Bass voice, Alto sax, Bari sax, Marimba, Piano, Organ, Drum set (2016) </li>
-	<li>Études for a Dying Race, Song Cycle – Soprano voice, Piano (2017)</li>
-
-</ul>	
-
-<h2 class="uk-heading-line"><span>Choral Music</span></h2>
-
-<ul class="uk-list uk-list-striped">
-<li>Expansion – SATB choir, Snare drum (2015)</li>
-<li>Sonnet 30 – SSA choir, Piano (2015) </li>
-<li>Like a Geometer - SATB choir, Organ (2018) or SATB choir, Live electronics</li>
-</ul>
-
-<h2 class="uk-heading-line"><span>Orchestra</span></h2>
-
-<ul class="uk-list uk-list-striped">
-<li>Paraworlds II – Symphony orchestra (2016)</li>
-<li>Eurobsession - Symphony Orchestra (2017)</li>
-</ul>
-
-<h2 class="uk-heading-line"><span>Rock Band</span></h2>
-
-<ul class="uk-list uk-list-striped">
-<li>Thoughts Past Pondered, album (2012) </li>
-<li>In The Round, album (2014) </li>
-<li>Mosaic Suite, multimedia (2017)</li>
-</ul>
-
-<h2 class="uk-heading-line"><span>Wind Ensemble</span></h2>
-
-<ul class="uk-list uk-list-striped">
-<li>Sun Shower Concertino – Saxophone quartet with Wind ensemble (2016) </li>
-</ul>
+	<h2 class="uk-heading-line"><span>Vocal</span></h2>
+	<ul class="uk-list uk-list-striped">
+		<li>Who Killed Adriana, Opera (co-composed) – 13 Singers, Chamber Orchestra (2018)</li>
+		<li>Études for a Dying Race, Song Cycle – Soprano, Piano (2017)</li>
+		<li>I Hope I Shall Arrive Soon, Chamber Opera – Soprano, Mezzo-Soprano,</li>
+		<li>Counter-Tenor, Tenor, Bass, Alto Saxophone, Baritone Saxophone,</li>
+		<li>Marimba, Piano, Organ, Drum Set (2016)</li>
+		<li>Farewell to Nova Scotia, Monodrama – Tenor, Violin, Piano (2016)</li>
+	</ul>
+	
 
 <?php
 echo file_get_contents( "mainPhp/main_layout_bottom.php" ); 
--- news-events.php
+++ news-events.php
@@ -2,31 +2,99 @@
 echo file_get_contents( "mainPhp/main_layout_top.php" ); 
 ?>
 
-	<h1 class="uk-heading-primary">News & Events</h1>
+	<h1 class="uk-heading-primary">News</h1>
 
-		"Zabathon III" http://winnipeg.carpediem.cd/events/6148680-zabathon-iii-at-crescent-fort-rouge-united-church/
+	<div class="uk-width-1-1 uk-width-2-3@m">
 
-<hr>
+	<hr>	
 
-"Zabathon II: 
-Multi-Genre Music Festival Features Opera Based on Philip K Dick Short Story"
-http://www.communitynewscommons.org/culture/arts-entertainment/zabathon-ii-multi-genre-music-festival-features-opera-based-on-philip-k-dick-short-story/
+	<article class="uk-article">
+	    <h1 class="uk-article-title">Residency with MacMillan Singers</h1>
+	   
+	    <p>On October 2nd I was announced the 2018-2019 Composer in Residence for the University of Toronto’s MacMillan Singers (SATB choir), an opportunity I applied for in early September. </p>
 
-<hr>
+	</article>
 
-"U of M Based Composer Showcasing Work at Zabathon II"
-https://www.classic107.com/news/u-of-m-based-composer-showcasing-work-at-zabathon-ii
+	<hr>
 
-<hr>
+	<article class="uk-article">
+	    <h1 class="uk-article-title">Who Killed Adriana <p class="uk-article-meta">Opera</p></h1>
 
-"Tristan Zaba and the Zabathon"
-http://winnipegmusicproject.com/winnipegmusicproject/2016/1/tristan-zaba-and-the-zabathon
+	    <p>As part of my master’s degree in composition, which I am currently pursuing at the University of Toronto, I	am working with a few other graduate students co-composing an opera on Michael Albano’s comedic libretto “Who Killed Adriana”. It will be premiered fully staged with orchestra by the U of T opera program in the new year. I am responsible for composing the overture, a portion of the opening scene, and the final scene.</p>
 
-<hr>
+	    <p>More information: 
+	    	<a class="uk-link-muted" href="http://operacanada.ca/event/university-of-toronto-faculty-of-music-who-killed-adriana/">
+	    	http://operacanada.ca/event/university-of-toronto-faculty-of-music-who-killed-adriana</a></p>
 
-For upcoming shows and events, visit our Manitoba Music page (manitobamusic.com/zaba) or look for our regular updates at:
+	</article>
 
-www.facebook.com/tristanzababand
+	<hr>
+
+	<article class="uk-article">
+	    <h1 class="uk-article-title">Matryoshki and a Blue Vase 
+	    	<p class="uk-article-meta">Piano Commission for Paul Williamson</p>
+	    </h1>
+	    
+	    <p>
+	    A recently finished piano piece written on commission for LA-based pianist Paul Williamson, one of CBC’s 2017 “30 Hot Classical Musicians under 30” and winner of numerous competitions. This piece was my main compositional focus from the beginning of this past summer until recently. </p>
+
+	    <p>Paul’s recording of Ligeti’s Piano Étude No. 1, “Désordre”:</p>
+	    <iframe width="560" height="315" src="https://www.youtube.com/embed/nZSD_tJp_DM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+	</article>
+
+	<hr>
+
+	<article class="uk-article">
+	    <h1 class="uk-article-title">AD Kastalsky 
+	    	<p class="uk-article-meta">International Choral Writing Competition</p>
+	    </h1>
+	    
+	    <p>I was recently announced a winner of the 2018 AD Kastalsky International Choral Writing Competition for my piece, “Like a Geometer”. The competition is jointly hosted by the conservatories of St. Petersburg, Novgorod, and Moscow. </p>
+
+	    <p>Recording of “Like a Geometer": </p>
+	    <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/501644148&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+	</article>
+
+	<hr>
+
+	<article class="uk-article">
+	    <h1 class="uk-article-title">The Zaba Farewell Weekend
+	    </h1>
+	    
+	    <p>A busy weekend of playing the final three “Zaba” shows back to back from June 1-3, 2018. The "farewell lineup" featured myself on Guitar/Vocal/Bass Guitar, Isaac Zee on Violin (who last joined us for our David Bowie tribute in 2016), Cody Valentonis (of Winnipeg bands the Psychics and Campfire Sigh) on Bass Guitar, Kerrigan Salnikowski on Drums, and Eli Zaba (my cousin) on auxiliary percussion. </p>
+	</article>
+
+	<hr>
+
+	<article class="uk-article">
+	    <h1 class="uk-article-title">Zabathon III  
+	    	<p class="uk-article-meta">Video</p>
+	    </h1>
+	    
+	    <p>The finished video of the third and final Winnipeg “Zabathon" concert, which occurred on March 16th. </p>
+
+	    <!-- Video on Youtube: https://www.youtube.com/watch?v=8CYg6AJMqwA -->
+	    <iframe width="560" height="315" src="https://www.youtube.com/embed/8CYg6AJMqwA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+	</article>
+
+	<hr>
+
+	<article class="uk-article">
+	    <h1 class="uk-article-title">Mosaic Suite 
+	    	<p class="uk-article-meta">Video</p>
+	    </h1>
+	    
+	    <p>Over the course of this past winter veteran Winnipeg filmmaker Greg Hanec and I finished setting my rock album “Mosaic Suite” to video. This project was made possible by the University of Manitoba’s Desautels Faculty of Music, and was premiered with a live soundtrack at Zabathon III on March 16th.</p>
+
+	    <p>Link to buy album: 
+	    	<a class="uk-link-muted" href="https://store.cdbaby.com/cd/tristanzaba3">
+	    		https://store.cdbaby.com/cd/tristanzaba3
+	    	</a>
+    	</p>
+	</article>
+
+	</div>
 
 <?php
 echo file_get_contents( "mainPhp/main_layout_bottom.php" ); 
