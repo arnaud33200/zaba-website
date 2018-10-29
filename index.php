f21a8cca4bbab5cfd5ce55ca34806809f4a45b01
arnaud <arnaud33200@gmail.com>
2018-10-28 20:21:58 -0400

    setup index with navigation card and custom background;

--- index.php
+++ index.php
@@ -4,6 +4,50 @@ echo file_get_contents( "mainPhp/main_layout_top.php" );
 
 	<h1 class="uk-heading-primary">Home</h1>
 
+	<div class="uk-grid-small uk-child-width-1-2 uk-child-width-1-4@s uk-text-center uk-margin-large-top uk-margin-medium-bottom" uk-grid>
+
+		<div>
+			<a href="bio.php">
+			<div class="uk-margin uk-card uk-card-default uk-card-body uk-height-small uk-background-cover uk-background-top-right" 
+			style="background-image: url(images/nav-bio.jpg);">
+		    <div class="uk-overlay uk-overlay-default uk-position-bottom">
+                <p>Bio</p>
+            </div>
+		</div></a>
+	</div>
+
+		<div>
+			<a href="musical-works.php">
+			<div class="uk-margin uk-card uk-card-default uk-card-body uk-height-small uk-background-cover" 
+			style="background-image: url(images/nav-works.jpg);">
+		    <div class="uk-overlay uk-overlay-default uk-position-bottom">
+                <p>Works</p>
+            </div>
+			</div></a>
+	</div>
+
+		<div>
+			<a href="news-events.php">
+			<div class="uk-margin uk-card uk-card-default uk-card-body uk-height-small uk-background-cover" 
+			style="background-image: url(images/nav-news.jpg);">
+		    <div class="uk-overlay uk-overlay-default uk-position-bottom">
+                <p>News</p>
+            </div>
+			</div></a>
+	</div>
+
+		<div>
+			<a href="contact.php">
+			<div class="uk-margin uk-card uk-card-default uk-card-body uk-height-small uk-background-cover" 
+			style="background-image: url(images/nav-contact.jpg);">
+		    <div class="uk-overlay uk-overlay-default uk-position-bottom">
+                <p>Contact</p>
+            </div>
+			</div></a>
+	</div>
+
+    </div>
+
 	<div uk-grid>
 
 		<div class="uk-width-1-1 uk-push-1-1 uk-width-2-3@s">
@@ -24,12 +68,9 @@ echo file_get_contents( "mainPhp/main_layout_top.php" );
 			
     	</div>
 
-		<div class="uk-width-1-1 uk-width-1-3@s">
+		<!-- <div class="uk-width-1-1 uk-width-1-3@s">
 			<img class="" src="images/zaba-flute.jpg">
-		</div>
-
-    	
-
+		</div> -->
  
 	</div>
 
